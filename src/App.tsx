38f0b759afdd335e6e01cfcc3142ec9d713b0f74
Raul <rauldougbr@gmail.com>
2026-09-14 17:50:22 -0300

    fix(app): envolver aplicacao com QueryClientProvider no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient.ts';
 import { AppRoutes } from './routes/index.tsx';
 
 export const App: React.FC = () => {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
